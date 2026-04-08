1bc4ca340bae40fbdcd3dd83f236b0408cf9b606
Pranav-Singh-Devloper <pranavsinghprimo@gmail.com>
2026-04-09 00:31:01 +0530

    made create stream dto

--- server/src/modules/stream/dto/create-stream.dto.ts
+++ server/src/modules/stream/dto/create-stream.dto.ts
@@ -1,1 +1,11 @@
-/// d
+// ─────────────────────────────────────────────────────────
+// Create Stream DTO
+// ─────────────────────────────────────────────────────────
+
+import { IsString, IsNotEmpty } from 'class-validator';
+
+export class CreateStreamDto {
+  @IsString()
+  @IsNotEmpty()
+  title!: string;
+}
